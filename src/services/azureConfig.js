be1baab4b34d69e01d1b8da776a809412d6188cf
Carolina Alvim <caro.alvim@gmail.com>
2023-06-28 11:34:21 -0300

    Update azureConfig.js

--- src/services/azureConfig.js
+++ src/services/azureConfig.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-useless-concat */
 require('dotenv').config();
 
-const subscriptionKey = 'd49f3175dda14a61ac18dd08f5bb95ce';
-const endpoint = 'https://demo-demo-talent.cognitiveservices.azure.com/' + '/face/v1.0/detect';
+const subscriptionKey = 'process.env.KEY_SECURE';
+const endpoint = 'process.env.ENDPOINT';
 
 const axios = require('axios').default;
 
